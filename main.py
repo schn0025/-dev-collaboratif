cdea48ac5a03d0aab57ed7642e6b220c60bb33e5
Émilia Beguin <emilia.beguin@etudiant.univ-reims.fr>
2022-09-30 11:48:41 +0200

    Ajout tests

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("******** Programme principal ********")
+from functions import convert
+
+print(convert(120, 8))
+print(convert(10, 2))
+print(convert(44, 5))
+print(convert(20, 8))
+print(convert(18, 2))
+print(convert(54, 5))
+print(convert(108, 8))
+print(convert(102, 2))
+print(convert(404, 5))
+print(convert(2022, 9))

b73bb7407647c1dc6d8a965626540fd8c4d13084
Arthur Schneider <arthur.schneider@etudiant.univ-reims.fr>
2022-10-05 14:58:40 +0200

    ajour de la license BSD

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Émilia BEGUIN
+# Copyright 2021 Émilia BEGUIN Arthur Schneider
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
